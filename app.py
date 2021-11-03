38002f28bba9c13b1d5a12d268a70a03544c3f83
Gawondal <fleurivalgawondallafrance15@gmail.com>
2021-11-03 13:47:33 -0400

    added post route to app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False, unique=True)
     author = db.Column(db.String, nullable=False)
-    review = db.Column(db.String(144), nullable=False)
+    review = db.Column(db.String(144), nullable=True)
     genre = db.Column(db.String, nullable=True)
 
     def __init__(self, title, author, review, genre):
@@ -29,9 +29,31 @@ class BookSchema(ma.Schema):
     class Meta:
         fields: ("id", "title", "author", "review", "genre")
 
-  
 book_schema = BookSchema()
 multiple_book_schema = BookSchema(many=True)
+
+@app.route("/book/add", methods=["POST"])
+def add_book():
+    if request.content_type != "application/json":
+        return jsonify('Error: Data must be JSON.')
+
+    post_data = request.get_json()
+    title = post_data.get('title')
+    author = post_data.get('author')
+    review = post_data.get('review')
+    genre = post_data.get('genre')
+
+    if title == None:
+       return jsonify("Error: Data must have a 'Title' key.")
+
+    if author == None:
+      return jsonify("Error: Data must have an 'Author' key.")   
+
+    new_book = Book(title, author , review, genre)
+    db.session.add(new_book)
+    db.session.commit()
+
+    return jsonify("Good job on adding a book!!")
 
 
 if __name__ == "__main__":      
